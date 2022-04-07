ac3304dc28e635795f1337d7d2a24a593a7f6840
Mike Levenick <85573255+mikelevenick@users.noreply.github.com>
2022-04-07 13:06:27 -0500

    Fixed whitespace

--- jamf_pro_extension_attributes/jamf_protect_last_check_in.sh
+++ jamf_pro_extension_attributes/jamf_protect_last_check_in.sh
@@ -10,8 +10,8 @@
 jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 
 if [ -f "$jamfProtectBinaryLocation" ]; then
-    plist=$($jamfProtectBinaryLocation info --plist)
-    jamfProtectLastCheckin=$(/usr/libexec/PlistBuddy -c "Print LastCheckin" /dev/stdin <<<"$plist")
+	plist=$($jamfProtectBinaryLocation info --plist)
+	jamfProtectLastCheckin=$(/usr/libexec/PlistBuddy -c "Print LastCheckin" /dev/stdin <<<"$plist")
 else
 	jamfProtectLastCheckin="Protect binary not found"
 fi
