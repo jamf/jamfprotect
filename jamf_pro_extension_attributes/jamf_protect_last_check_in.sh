1e8c65b2ba75a091b9790702e3d97fd7427429f2
Mike Levenick <mike.levenick@jamf.com>
2022-05-09 13:37:57 -0500

    Updated variables to match Pro EA templates.

--- jamf_pro_extension_attributes/jamf_protect_last_check_in.sh
+++ jamf_pro_extension_attributes/jamf_protect_last_check_in.sh
@@ -12,10 +12,10 @@ jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 if [[ -f "$jamfProtectBinaryLocation" ]]; then
 	plist=$($jamfProtectBinaryLocation info --plist)
     xpath="/plist/dict/date[preceding-sibling::key='LastCheckin'][1]/text()"
-    jamfProtectLastCheckin=$( echo $plist | /usr/bin/xpath -e "${xpath}" 2>/dev/null)
-    result=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$jamfProtectLastCheckin" "+%Y-%m-%d %H:%M:%S")
+    rawLastCheckin=$(/bin/echo $plist | /usr/bin/xpath -e "${xpath}" 2>/dev/null)
+    jamfProtectLastCheckin=$(/bin/date -j -f "%Y-%m-%dT%H:%M:%SZ" "$rawLastCheckin" "+%Y-%m-%d %H:%M:%S")
 else
-	result="Protect binary not found"
+	jamfProtectLastCheckin="Protect binary not found"
 fi
 
-echo "<result>$result</result>"
+echo "<result>$jamfProtectLastCheckin</result>"
