03291f6c74cb7fa1e2ba7da436ad7d8ebd3a37ab
Allen Golbig <golbiga@gmail.com>
2022-07-12 14:50:46 -0400

    new api scripts

---
+++ jamf_protect_api/scripts/python/generate_device_controls_report.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+# This example Python script below does the following:
+# - Obtains an access token.
+# - Completes a listAlerts query request that returns all logs reported to Jamf
+#   Protect, with filtering by eventType auth-mount.
+# - Exports alert data in JSON format to Jamf_Protect_Device_Controls_alerts_{Date}.json
+# - Creates a CSV file
+
+# Keep the following in mind when using this script:
+# - You must define the PROTECT_INSTANCE, CLIENT_ID, and PASSWORD variables to
+#   match your Jamf Protect environment. The PROTECT_INSTANCE variable is your
+#   tenant name (eg. your-tenant), which is included in your tenant URL (eg.
+#   https://your-tenant.protect.jamfcloud.com).
+# - This script requires the 3rd party Python library 'requests'
+
+
+import sys, json, requests, csv, os, datetime as dt
+from datetime import datetime
+
+PROTECT_INSTANCE = ""
+CLIENT_ID = ""
+PASSWORD = ""
+
+JSON_OUTPUT_FILE = (
+    f"Jamf_Protect_Device_Controls_Alerts_{datetime.utcnow().strftime('%Y-%m-%d')}.json"
+)
+CSV_OUTPUT_FILE = (
+    f"Jamf_Protect_Device_Controls_Alerts_{datetime.utcnow().strftime('%Y-%m-%d')}.csv"
+)
+
+
+def get_access_token(protect_instance, client_id, password):
+    """Gets a reusable access token to authenticate requests to the Jamf
+    Protect API"""
+    token_url = f"https://{protect_instance}.protect.jamfcloud.com/token"
+    payload = {
+        "client_id": client_id,
+        "password": password,
+    }
+    resp = requests.post(token_url, json=payload)
+    resp.raise_for_status()
+    resp_data = resp.json()
+    print(
+        f"Access token granted, valid for {int(resp_data['expires_in'] // 60)} minutes."
+    )
+    return resp_data["access_token"]
+
+
+def make_api_call(protect_instance, access_token, query, variables=None):
+    """Sends a GraphQL query to the Jamf Protect API, and returns the
+    response."""
+    if variables is None:
+        variables = {}
+    api_url = f"https://{protect_instance}.protect.jamfcloud.com/graphql"
+    payload = {"query": query, "variables": variables}
+    print(payload)
+    headers = {"Authorization": access_token}
+    resp = requests.post(
+        api_url,
+        json=payload,
+        headers=headers,
+    )
+    print(resp)
+    resp.raise_for_status()
+    return resp.json()
+
+
+LIST_ALERTS_QUERY = """
+        query listAlerts(
+            $created_cutoff: AWSDateTime
+            $event_type: String
+            $page_size: Int 
+            $next: String
+        ) {
+            listAlerts(
+                input: {
+                    filter: {
+                        eventType: { equals: $event_type },
+                        and: {
+                        created: { greaterThan: $created_cutoff }
+                    }}
+                    pageSize: $page_size
+                    next: $next
+                }
+            ) {
+                items {
+                        json
+                        eventType
+                        computer {
+                            hostName
+                        }
+                        created
+                    }
+                        pageInfo {
+                                next
+                    }
+                }
+            }
+        """
+
+
+def __main__():
+
+    if not all([PROTECT_INSTANCE, CLIENT_ID, PASSWORD]):
+        print("ERROR: Variables PROTECT_INSTANCE, CLIENT_ID, and PASSWORD must be set.")
+        sys.exit(1)
+
+    # Get the access token
+    access_token = get_access_token(PROTECT_INSTANCE, CLIENT_ID, PASSWORD)
+
+    cutoff_days = 30
+    cutoff_date = dt.datetime.now() - dt.timedelta(days=cutoff_days)
+
+    results = []
+    next_token = None
+    page_count = 1
+
+    print("Retrieving paginated results:")
+
+    while True:
+        print(f"  Retrieving page {page_count} of results...")
+        vars = {
+            "event_type": "auth-mount",
+            "created_cutoff": cutoff_date.isoformat() + "Z",
+            "page_size": 100,
+            "next": next_token,
+        }
+        resp = make_api_call(PROTECT_INSTANCE, access_token, LIST_ALERTS_QUERY, vars)
+        next_token = resp["data"]["listAlerts"]["pageInfo"]["next"]
+        results.extend(resp["data"]["listAlerts"]["items"])
+        if next_token is None:
+            break
+        page_count += 1
+    print(f"Found {len(results)} alerts matching filter.\n")
+    print(f"Writing results to '{JSON_OUTPUT_FILE}'")
+    with open(JSON_OUTPUT_FILE, "w") as output:
+        json.dump(results, output, sort_keys=True, indent=4)
+
+    data_file = open(CSV_OUTPUT_FILE, "w", newline="")
+    csv_writer = csv.writer(data_file)
+    headers = [
+        "Timestamp",
+        "HostName",
+        "Serial",
+        "Vendor",
+        "Vendor ID",
+        "Product",
+        "Product ID",
+        "Device Serial",
+        "Encrypted",
+        "Action",
+    ]
+    csv_writer.writerow(headers)
+    with open(JSON_OUTPUT_FILE, "r") as f:
+        output = json.load(f)
+        for o in output:
+            json_list = json.loads(o["json"])
+            hostname = json_list["host"]["hostname"]
+            serial = json_list["host"]["serial"]
+            vendorname = json_list["match"]["event"]["device"]["vendorName"]
+            vendorid = json_list["match"]["event"]["device"]["vendorId"]
+            productname = json_list["match"]["event"]["device"]["productName"]
+            productid = json_list["match"]["event"]["device"]["productId"]
+            devicesn = json_list["match"]["event"]["device"]["serialNumber"]
+            isencrypted = json_list["match"]["event"]["device"]["isEncrypted"]
+            action = (
+                json_list["match"]["actions"][0]["name"]
+                + ", "
+                + json_list["match"]["actions"][1]["name"]
+                + " & "
+                + json_list["match"]["actions"][2]["name"]
+            )
+            time = json_list["match"]["event"]["timestamp"]
+            timestamp = datetime.utcfromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S")
+
+            rows = [
+                timestamp,
+                hostname,
+                serial,
+                vendorname,
+                vendorid,
+                productname,
+                productid,
+                devicesn,
+                isencrypted,
+                action,
+            ]
+
+            csv_writer.writerow(rows)
+    print(f"Writing results to '{CSV_OUTPUT_FILE}'")
+    f.close()
+    os.remove(JSON_OUTPUT_FILE)
+    data_file.close()
+    print("done")
+
+if __name__ == "__main__":
+    __main__()
---
+++ jamf_protect_api/scripts/python/update_alert_status.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+# This example Python script updates the status of alert(s) (New, InProgress, or Resolved).
+
+# The script does the following:
+#
+# - Obtains an access token.
+# - Updates the status of the provided alert(s), using the
+#   updateAlerts mutation.
+
+# Keep the following in mind when using this script:
+#
+# - You must define the PROTECT_INSTANCE, CLIENT_ID, and PASSWORD variables to
+#   match your Jamf Protect environment. The PROTECT_INSTANCE variable is your
+#   tenant name (eg. your-tenant), which is included in your tenant URL (eg.
+#   https://your-tenant.protect.jamfcloud.com).
+# - There are two required arguments that must be passed when running the script,
+#   -u/--uuid and -s/--status.
+# - This script requires the 3rd party Python library 'requests'
+
+import requests
+import argparse
+
+PROTECT_INSTANCE = ""
+CLIENT_ID = ""
+PASSWORD = ""
+
+
+def get_access_token(protect_instance, client_id, password):
+    """Gets a reusable access token to authenticate requests to the Jamf
+    Protect API"""
+
+    token_url = f"https://{protect_instance}.protect.jamfcloud.com/token"
+
+    payload = {
+        "client_id": client_id,
+        "password": password,
+    }
+
+    resp = requests.post(token_url, json=payload)
+    resp.raise_for_status()
+
+    resp_data = resp.json()
+    print(
+        f"Access token granted, valid for {int(resp_data['expires_in'] // 60)} minutes."
+    )
+
+    return resp_data["access_token"]
+
+
+def make_api_call(protect_instance, access_token, query, variables=None):
+    """Sends a GraphQL query to the Jamf Protect API, and returns the
+    response."""
+    if variables is None:
+        variables = {}
+    api_url = f"https://{protect_instance}.protect.jamfcloud.com/graphql"
+    payload = {"query": query, "variables": variables}
+    headers = {"Authorization": access_token}
+    resp = requests.post(
+        api_url,
+        json=payload,
+        headers=headers,
+    )
+    resp.raise_for_status()
+    return resp.json()
+
+
+UPDATE_ALERT_STATUS_QUERY = """
+    mutation updateAlerts($input: AlertUpdateInput!){
+        updateAlerts(input: $input) {
+            items {
+                status
+            }
+        }
+    }
+"""
+
+
+def create_args():
+    parser = argparse.ArgumentParser(
+        description="Given an Alert(s) UUID(s), set the current status."
+    )
+    parser.add_argument(
+        "-u",
+        "--uuid",
+        default=None,
+        help="UUIDs of alerts, comma separated.",
+        required=True,
+    )
+    parser.add_argument(
+        "-s", "--status", default=None, help="Status of alerts.", required=True
+    )
+    return parser.parse_args()
+
+
+def __main__():
+
+    args = create_args()
+
+    ALERT_UUID = args.uuid.split(",")
+    ALERT_STATUS = args.status
+
+    if (
+        ALERT_STATUS == "New"
+        or ALERT_STATUS == "InProgress"
+        or ALERT_STATUS == "Resolved"
+    ):
+
+        # Get the access token
+        access_token = get_access_token(PROTECT_INSTANCE, CLIENT_ID, PASSWORD)
+
+        # Set variables for graphql mutation
+        variables = {
+            "input": {"uuids": ALERT_UUID, "status": ALERT_STATUS},
+        }
+
+        # Make API call
+        resp = make_api_call(
+            PROTECT_INSTANCE, access_token, UPDATE_ALERT_STATUS_QUERY, variables
+        )
+        print(resp)
+        print(f"The status of alert(s) {ALERT_UUID} are set to {ALERT_STATUS}.")
+    else:
+        print("Status must be set to New, InProgress, or Resolved.")
+        return
+
+
+if __name__ == "__main__":
+
+    __main__()
