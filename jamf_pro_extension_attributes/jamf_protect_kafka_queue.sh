7af0507d49443e36ec3365517188a4788d769b2b
Thijs Xhaflaire <thijsxhaflaire31@hotmail.com>
2024-09-16 08:46:33 +0200

    Adding Jamf Pro EAs for monitoring Kafka and SysLog queues

---
+++ jamf_pro_extension_attributes/jamf_protect_kafka_queue.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This Extension Attribute will report on the SysLog Queue of Jamf Protect
+#
+# Data Type: Integer
+# Input Type: Script
+#
+##### Script starts here #####
+
+#Jamf Protect Location
+jamfProtectBinaryLocation="/usr/local/bin/protectctl"
+
+if [ -f "$jamfProtectBinaryLocation" ]; then
+  plist=$($jamfProtectBinaryLocation info --plist)
+  jamfProtectHTTPQueue=$(/usr/libexec/PlistBuddy -c "Print UploadQueue:SysLog" /dev/stdin <<<"$plist")
+else
+  jamfProtectHTTPQueue="Protect binary not found"
+fi
+
+echo "<result>$jamfProtectHTTPQueue</result>"
